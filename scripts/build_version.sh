2125b2831a8ca5cda9a28845b89b4532f271c33a
Bear Riley <46008563+BehrRiley@users.noreply.github.com>
2020-11-15 19:12:26 -0500

    Implement further build cleaning
    - pulls from origin before initiate
    - cleans build root directory better
    - removes untracked content
    - consistency updates for script

--- scripts/build_version.sh
+++ scripts/build_version.sh
@@ -2,22 +2,27 @@
 #Date=$(date +%Y-%m-%d-%H-%M-%S)
 
 cd ..
+git pull
+
+# ██ [ clean build directory ] ██
 if [ ! -d build ]; then
   mkdir build
 else
   rm -r build
 fi
 
+# ██ [ remove previous build ] ██
 if [ -f build.zip ]; then
     rm build.zip
 fi
 
+# ██ [ compile build ] ██
 for f in $(find . -type f -a -not -path './.git/*'); do
     if ! $(git check-ignore -q $f); then
       #echo "Copying file: $f"
       file="${f#*.}"
       dir="${file%/*}"
-      echo $file
+      echo "Adding $file"
       #echo $dir
       if [ ! -d "./build/$dir" ]; then
         echo "Creating directory: $dir"
@@ -28,9 +33,25 @@ for f in $(find . -type f -a -not -path './.git/*'); do
     fi
 done
 
+# ██ [ clean untracked files ] ██
+for f in $(git ls-files -o --exclude-standard); do
+  rm ./build/$f
+  echo ./build/$f
+done
+
+# ██ [ reorganize root ] ██
+rm -r build/scripts
+for f in README.md mv CONTRIBUTING.md LICENSE; do
+  mv build/$f build/meta/
+done
+
+# ██ [ clean empty directories ] ██
 find build/ -type d -exec rmdir {} + 2>/dev/null
 
+# ██ [ finish new build ] ██
 cd build
 zip -r ../build.zip *
 
+# ██ [ clean working directory ] ██
+cd ..
 rm -r build
